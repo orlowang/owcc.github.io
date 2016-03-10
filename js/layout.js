8ac8db41300fab56485fc5557b4e9b923b056371
Orlo Wang <ow@Orlo.local>
2016-03-11 01:31:55 +0800

    fix

--- js/layout.js
+++ js/layout.js
@@ -1,6 +1,5 @@
 var menu = document.querySelector('.menu-icon'),
-		side = document.querySelector('.site-left'),
-		body = document.querySelector('body>.wrapper');
+		side = document.querySelector('.site-left');
 console.log(menu)
 menu.addEventListener('touchstart', function() {
 	var point = side.getAttribute('open')
@@ -8,12 +7,10 @@ menu.addEventListener('touchstart', function() {
 
 	switch (point) {
 		case 'opened':
-			body.style.width = '100%'
 			side.style.marginLeft = '-200px'
 			side.setAttribute('open', 'closed')
 			break;
 		default:
-			body.style.width = 'calc(100% + 200px)'
 			side.style.marginLeft = 0
 			side.setAttribute('open', 'opened')
 			break;

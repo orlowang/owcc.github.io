5118f23431f60da2df17c9673de2d610f3321fc2
Orlo Wang <ow@Orlo.local>
2016-03-11 03:39:23 +0800

    update

--- js/layout.js
+++ js/layout.js
@@ -8,20 +8,16 @@ menu.addEventListener('touchstart', function() {
 
 	switch (point) {
 		case 'opened':
-			side.style.boxShadow = 'none'
 			side.style.marginLeft = '-200px'
+			side.style.boxShadow = 'none'
 			body.style.overflow = 'auto'
 			side.setAttribute('id', 'closed')
-			menu.classList.remove('on')
-			body.style.webkitFilter = 'none'
 			break;
 		default:
-			body.style.webkitFilter = 'blur(5px)'
 			side.style.boxShadow = '10px 0 20px rgba(0,0,0,.3)'
 			side.style.marginLeft = 0
 			body.style.overflow = 'hidden'
 			side.setAttribute('id', 'opened')
-			menu.classList.add('on')
 			break;
 	}
 })

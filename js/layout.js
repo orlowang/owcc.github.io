34367146ad1b6100b7632628633ea4b83c7b9ab4
Orlo Wang <ow@Orlo.local>
2016-03-11 00:30:20 +0800

    update mobile view

--- js/layout.js
+++ js/layout.js
@@ -2,17 +2,17 @@ var menu = document.querySelector('.menu-icon'),
 		side = document.querySelector('.site-left');
 console.log(menu)
 menu.addEventListener('touchstart', function() {
-	var point = menu.getAttribute('open')
+	var point = side.getAttribute('open')
 	side.style.marginLeft = 0
 
 	switch (point) {
 		case 'opened':
 			side.style.marginLeft = '-200px'
-			menu.setAttribute('open', 'closed')
+			side.setAttribute('open', 'closed')
 			break;
 		default:
 			side.style.marginLeft = 0
-			menu.setAttribute('open', 'opened')
+			side.setAttribute('open', 'opened')
 			break;
 	}
 })

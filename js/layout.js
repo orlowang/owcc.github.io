f877a0cf68ac8875aa54d83dcd3f2313d0dfcd74
Orlo Wang <ow@Orlo.local>
2016-03-11 01:52:21 +0800

    fix

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,6 @@
 var menu = document.querySelector('.menu-icon'),
-		side = document.querySelector('.site-left');
+		side = document.querySelector('.site-left'),
+		body = document.querySelector('section.paper');
 console.log(menu)
 menu.addEventListener('touchstart', function() {
 	var point = side.getAttribute('open')
@@ -9,11 +10,13 @@ menu.addEventListener('touchstart', function() {
 		case 'opened':
 			side.style.boxShadow = 'none'
 			side.style.marginLeft = '-200px'
+			body.style.overflow = 'auto'
 			side.setAttribute('open', 'closed')
 			break;
 		default:
 			side.style.boxShadow = '10px 0 20px rgba(0,0,0,.3)'
 			side.style.marginLeft = 0
+			body.style.overflow = 'hidden'
 			side.setAttribute('open', 'opened')
 			break;
 	}

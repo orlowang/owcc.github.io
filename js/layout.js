35227ab9744db410be293f65c131c4368b11baea
Orlo Wang <ow@Orlo.local>
2016-03-11 23:20:34 +0800

    update

--- js/layout.js
+++ js/layout.js
@@ -12,10 +12,10 @@ function handle() {
 	switch (point) {
 		case 'opened':
 			side.style.marginLeft = '-200px'
+			menu.style.right = '-44px'
+			side.setAttribute('id', 'closed')
 			side.style.boxShadow = 'none'
 			body.style.overflow = 'auto'
-			side.setAttribute('id', 'closed')
-			menu.style.right = '-44px'
 			break;
 		default:
 			side.style.boxShadow = '10px 0 20px rgba(0,0,0,.3)'

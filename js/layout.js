85f804a71ebb1f858b52cdd013b025b97af8c67a
Orlo Wang <ow@Orlo.local>
2016-03-12 11:01:19 +0800

    update post

--- js/layout.js
+++ js/layout.js
@@ -1,9 +1,17 @@
-var menu = document.querySelector('.menu-icon'),
-		side = document.querySelector('.site-left'),
-		body = document.querySelector('section.paper');
+var menu  = document.querySelector('.menu-icon'),
+		side  = document.querySelector('.site-left'),
+		body  = document.querySelector('section.paper'),
+		title = document.querySelector('section.title>span'),
+		head  = document.querySelector('.post-header');
 
 menu.addEventListener('touchstart', function() {
 	menu.addEventListener('touchend', handle)
+})
+
+body.addEventListener('scroll', function() {
+	var headHight = window.getComputedStyle(head).height;
+
+	title.style.opacity = body.scrollTop >= parseInt(headHight) ? 1 : 0
 })
 
 function handle() {

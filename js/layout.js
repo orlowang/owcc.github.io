9c2ce9b11890fb81d5983c87d0761deb17c5db49
Orlo Wang <ow@Orlo.local>
2016-03-16 08:21:08 +0800

    update

--- js/layout.js
+++ js/layout.js
@@ -19,18 +19,10 @@ function handle() {
 
 	switch (point) {
 		case 'opened':
-			side.style.marginLeft = '-200px'
-			menu.style.right = '-44px'
 			side.setAttribute('id', 'closed')
-			side.style.boxShadow = 'none'
-			body.style.overflow = 'auto'
 			break;
 		default:
-			side.style.boxShadow = '10px 0 20px rgba(0,0,0,.3)'
-			side.style.marginLeft = 0
-			body.style.overflow = 'hidden'
 			side.setAttribute('id', 'opened')
-			menu.style.right = 0
 			break;
 	}
 }
